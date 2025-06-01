6665eeda4b22fed15beb17182d77fac8b521065e
Mourn5367 <qwerrrt6@gmail.com>
2025-06-01 18:28:06 +0900

    ip 수정정

--- frontend/lib/config/api_config.dart
+++ frontend/lib/config/api_config.dart
@@ -3,7 +3,8 @@ class ApiConfig {
   // Docker 환경에서는 서비스 이름을 사용
   static String get baseUrl {
     // 웹 브라우저에서 직접 접근하는 경우 window.location.hostname 사용
-    return 'http://localhost:5000/api';
+    //return 'http://localhost:5000/api';
+     return 'http://192.168.137.1:5000/api';
   }
   
   static const String menuEndpoint = '/menu';
