99b4f8b935d443f028400a8ca06ce97a3bfd9a39
Mourn5367 <qwerrrt6@gmail.com>
2025-06-26 00:17:49 +0900

    수정정

--- frontend/lib/config/api_config.dart
+++ frontend/lib/config/api_config.dart
@@ -11,29 +11,6 @@
 //   static const String postsEndpoint = '/posts';
 // }
 
-// class ApiConfig {
-//   static String get baseUrl {
-//     const bool kIsWeb = identical(0, 0.0);
-//
-//     if (kIsWeb) {
-//       // 웹에서는 현재 호스트의 5000 포트 사용
-//       return '${Uri.base.origin.replaceAll(':80', '')}:5000/api';
-//     } else {
-//       // 모바일에서는 실제 서버 IP 사용
-//       // 여기를 실제 서버 IP로 변경하세요!
-//       const serverIp = String.fromEnvironment('192.168.26.165',
-//           defaultValue: '192.168.26.165'); // localhost 대신 실제 IP
-//       return 'http://$serverIp:5000/api';
-//     }
-//   }
-//
-//   static const String menuEndpoint = '/menu';
-//   static const String postsEndpoint = '/posts';
-// }
-
-// frontend/lib/config/api_config.dart - IP 주소 수정
-
-
 class ApiConfig {
   static String get baseUrl {
     const bool kIsWeb = identical(0, 0.0);
@@ -43,9 +20,9 @@ class ApiConfig {
       return '${Uri.base.origin.replaceAll(':80', '')}:5000/api';
     } else {
       // 모바일에서는 실제 서버 IP 사용
-      // 여기를 현재 PC의 실제 IP로 변경
-      const serverIp = String.fromEnvironment('SERVER_IP',
-          defaultValue: '192.168.0.2'); // 현재 PC의 IP 주소로 변경
+      // 여기를 실제 서버 IP로 변경하세요!
+      const serverIp = String.fromEnvironment('192.168.26.165',
+          defaultValue: '192.168.26.165'); // localhost 대신 실제 IP
       return 'http://$serverIp:5000/api';
     }
   }
@@ -53,3 +30,24 @@ class ApiConfig {
   static const String menuEndpoint = '/menu';
   static const String postsEndpoint = '/posts';
 }
+
+// frontend/lib/config/api_config.dart - IP 주소 수정
+// class ApiConfig {
+//   static String get baseUrl {
+//     const bool kIsWeb = identical(0, 0.0);
+
+//     if (kIsWeb) {
+//       // 웹에서는 현재 호스트의 5000 포트 사용
+//       return '${Uri.base.origin.replaceAll(':80', '')}:5000/api';
+//     } else {
+//       // 모바일에서는 실제 서버 IP 사용
+//       // 여기를 현재 PC의 실제 IP로 변경
+//       const serverIp = String.fromEnvironment('SERVER_IP',
+//           defaultValue: '192.168.0.2'); // 현재 PC의 IP 주소로 변경
+//       return 'http://$serverIp:5000/api';
+//     }
+//   }
+
+//   static const String menuEndpoint = '/menu';
+//   static const String postsEndpoint = '/posts';
+// }
